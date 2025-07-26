43a3e6aa059a83b6715a6bc58fe50aad52edaddc
thuudao <thudao9600@gmail.com>
2025-07-26 16:04:37 +0700

    Create bai_41.py

---
+++ bai_41.py
@@ -0,0 +1,11 @@
+n = float(input())
+if n >= 9.0:
+    print("Xuat sac")
+elif n >= 8.0:
+    print("Gioi")
+elif n >= 6.5:
+    print("Kha")
+elif n >= 5.0:
+    print("Trung binh")
+else:
+    print("Yeu")
